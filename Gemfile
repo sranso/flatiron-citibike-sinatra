49ffd750ba2bb569322b89da3551644fbdcd007e
sranso <sranso@gmail.com>
2014-03-27 14:03:38 -0400

    changes

--- Gemfile
+++ Gemfile
@@ -3,6 +3,7 @@ source 'https://rubygems.org'
 gem 'sinatra'
 gem 'multi_json'
 gem 'sinatra-contrib'
+gem 'json'
 
 group :development do
   gem 'shotgun'
--- app.rb
+++ app.rb
@@ -17,10 +17,28 @@ module Citibike
     before do
       json = File.open("data/citibikenyc.json").read
       @data = MultiJson.load(json) # a ruby hash of the data from JSON file
+      @data.each do |hash|
+        hash["lng"] = hash["lng"]/1000000.to_f
+        hash["lat"] = hash["lat"]/1000000.to_f
+      end
     end
 
     get '/' do
       erb :home # will print out the contents of @data
+    end
+
+    get '/form' do
+      erb :form
+    end
+
+    post '/map' do
+      @start = params["start"]
+      @end = params["end"]
+      erb :map
+    end
+
+    get '/map' do
+      erb :map
     end
 
   end
